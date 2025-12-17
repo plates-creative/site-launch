79245e3a27cbe68b53874409982f8a394d7a96fe
Plates Studio <antonio@plates.studio>
2025-12-17 08:49:23 -0600

    trying to turn the volume up to 11

--- experiments/emergence/audio.js
+++ experiments/emergence/audio.js
@@ -39,7 +39,7 @@ const bellDelay2 = new Tone.PingPongDelay({
 const limiter = new Tone.Limiter(-1);
 const fxBus = new Tone.Gain(1);
 
-const master = new Tone.Gain(1.25); // start at 1.15–1.35
+const master = new Tone.Gain(1.6); // start at 1.15–1.35
 fxBus.chain(tapeChorus, tapeVibrato, delay, reverb, limiter, master, Tone.Destination);
 
 
